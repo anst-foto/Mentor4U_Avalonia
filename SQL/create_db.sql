f8905ceddd0ca7931b973f9f8d5add62f05e3549
Starinin Andrey <starininandrey@gmail.com>
2024-05-11 14:13:43 +0300

    Update create_db.sql

--- SQL/create_db.sql
+++ SQL/create_db.sql
@@ -3,10 +3,49 @@
 -- DROP DATABASE IF EXISTS mentor_db;
 -- CREATE DATABASE mentor_db;
 
--- DROP SCHEMA test;
--- CREATE SCHEMA test;
+-- ######################################
+
+-- DROP SCHEMA log;
+-- CREATE SCHEMA log;
+-- SET SCHEMA 'log';
+
+--Create tables
+CREATE TABLE table_logs(
+	id SERIAL PRIMARY KEY,
+	date DATE NOT NULL DEFAULT CURRENT_DATE,
+	time TIME NOT NULL DEFAULT CURRENT_TIME,
+	event_name TEXT NOT NULL,
+	table_name TEXT NOT NULL,
+	column_name TEXT NULL,
+	old_value TEXT NULL,
+	new_value TEXT NULL,
+	description TEXT NOT NULL
+);
+
+-- Create procedures
+CREATE PROCEDURE procedure_logger(
+	IN event TEXT, 
+	IN table_name TEXT, 
+	IN description TEXT,
+	IN column_name TEXT DEFAULT NULL, 
+	IN old_value TEXT DEFAULT NULL, 
+	IN new_value TEXT DEFAULT NULL
+	)
+LANGUAGE sql
+BEGIN ATOMIC
+	INSERT INTO table_logs (event_name, table_name, column_name, old_value, new_value, description)
+	VALUES (event, table_name, column_name, old_value, new_value, description);
+END;
+
+--CALL procedure_logger('TEST', 'test', 'test');
+--CALL procedure_logger('TEST', 'test', 'test', 'column', 'old', 'new');
+--SELECT * FROM table_logs;
 
 -- ######################################
+
+-- DROP SCHEMA test;
+-- CREATE SCHEMA test;
+-- SET SCHEMA 'test';
 
 -- Create tables
 CREATE TABLE table_roles (
@@ -55,3 +94,14 @@ CREATE TABLE table_persons_specializations (
         ON DELETE NO ACTION
         ON UPDATE NO ACTION
 );
+
+
+
+-- Create triggers
+CREATE TRIGGER trigger_insert_log
+	AFTER INSERT ON test.table_roles
+	FOR EACH ROW
+    EXECUTE PROCEDURE log.procedure_logger('INSERT', 'table_roles', 'NEW');
+
+-- CALL log.procedure_logger('INSERT', 'table_roles', 'NEW');
+-- SELECT * FROM log.table_logs;
