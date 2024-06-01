9172c9aa998a2f3a8c8ff5b816595e27a2ea0bda
Starinin Andrey <starininandrey@gmail.com>
2024-06-01 13:02:21 +0300

    Создание тестов для класса Roles

---
+++ Mentor4U_Avalonia.DAL.Test/RolesTest.cs
@@ -0,0 +1,32 @@
+﻿using Mentor4U_Avalonia.Models;
+
+namespace Mentor4U_Avalonia.DAL.Test;
+
+public class RolesTest
+{
+    private const string ConnectionString = "Host=localhost;Port=5432;Database=mentor_db;User ID=postgres;Password=1234;Pooling=true;SearchPath=test;";
+    
+    [Fact]
+    public async Task GetAsync_PositiveTest()
+    {
+        var roles = new Roles(ConnectionString);
+        var actual = await roles.GetAsync(1);
+
+        var expected = new Role()
+        {
+            Id = 1,
+            RoleName = "admin"
+        };
+        
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async Task GetAsync_NegativeTest()
+    {
+        var roles = new Roles(ConnectionString);
+        var actual = await roles.GetAsync(0);
+        
+        Assert.Null(actual);
+    }
+}
