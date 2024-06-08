fdaeabf65978130210a0af30b2bfd07ef1c19067
Starinin Andrey <starininandrey@gmail.com>
2024-06-08 12:57:28 +0300

    Добавление логгирования ошибок в BLL.Roles.cs

--- Mentor4U_Avalonia.BLL/Roles.cs
+++ Mentor4U_Avalonia.BLL/Roles.cs
@@ -1,30 +1,60 @@
-﻿using Mentor4U_Avalonia.DAL;
+﻿using Logger;
+using Logger.File;
+using Mentor4U_Avalonia.DAL;
 using Mentor4U_Avalonia.Models;
 
 namespace Mentor4U_Avalonia.BLL;
 
 public class Roles
 {
+    private readonly ILogger? _logger;
+    
     private readonly ICrud<Role> _data;
 
     public Roles(ICrud<Role> data)
     {
         _data = data;
+
+        _logger = new LogToFile();
     }
 
     public async Task<Role?> GetByIdAsync(int id)
     {
-        return await _data.GetAsync(id);
+        try
+        {
+            return await _data.GetAsync(id);
+        }
+        catch (Exception e)
+        {
+            _logger?.Error($"Module: {nameof(BLL.Roles)}. Method: {nameof(GetByIdAsync)}. Message:  {e.Message}");
+            throw;
+        }
     }
 
     public async Task<Role?> GetByNameAsync(string name)
     {
-        return (await _data.GetAllAsync() ?? Array.Empty<Role>())
-            .SingleOrDefault(r => r.RoleName == name);
+        try
+        {
+            return (await _data.GetAllAsync() ?? Array.Empty<Role>())
+                .SingleOrDefault(r => r.RoleName == name);
+        }
+        catch (Exception e)
+        {
+            _logger?.Error($"Module: {nameof(BLL.Roles)}. Method: {nameof(GetByNameAsync)}. Message:  {e.Message}");
+            throw;
+        }
     }
 
     public async Task<IEnumerable<Role>?> GetAllAsync()
     {
-        return await _data.GetAllAsync();
+        try
+        {
+            return await _data.GetAllAsync();
+        }
+        catch (Exception e)
+        {
+            _logger?.Error($"Module: {nameof(BLL.Roles)}. Method: {nameof(GetAllAsync)}. Message:  {e.Message}");
+            throw;
+        }
     }
 }
